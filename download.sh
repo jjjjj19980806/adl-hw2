dc843086cb2667dcb3bf645c5bc4959196cc8c84
jjjjj19980806 <jjjjj19980806@gmail.com>
2021-05-09 20:27:00 +0800

    add macbert-large download link

--- download.sh
+++ download.sh
@@ -1,2 +1,7 @@
-wget https://www.dropbox.com/s/7ju74vwqt0orofq/ckpt-chinese-roberta-wwm-ext.zip?dl=1 -O ckpt-chinese-roberta-wwm-ext.zip
-unzip ckpt-chinese-roberta-wwm-ext.zip
+# model macbert-large (100% training set)
+wget https://www.dropbox.com/s/zofsvimpuh5t5ge/ckpt-chinese-macbert-large-all.zip?dl=1 -O ckpt-chinese-macbert-large-all.zip
+unzip ckpt-chinese-macbert-large-all.zip
+
+# model chinese-roberta-wwm-ext (90% training set)
+# wget https://www.dropbox.com/s/7ju74vwqt0orofq/ckpt-chinese-roberta-wwm-ext.zip?dl=1 -O ckpt-chinese-roberta-wwm-ext.zip
+# unzip ckpt-chinese-roberta-wwm-ext.zip
